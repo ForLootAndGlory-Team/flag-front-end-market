d0d6b05992f64849b4f80b3ec015d56d39132602
Drikkx <88060062+Drikkx@users.noreply.github.com>
2025-01-18 10:38:46 +0100

    update polyfill for crypto external module

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,11 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 export default defineConfig({
 	plugins: [react(),
+		nodePolyfills(),
 	VitePWA({
 		registerType: 'autoUpdate',
 		includeAssets: ['favicon.ico'],
